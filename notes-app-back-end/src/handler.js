29126a256a1a12b7bd19da2eb9db7637083df7a9
Okyan Awang Ramadhana <okyanawang@gmail.com>
2023-03-26 06:58:27 +0700

    Create Post Note Handler

---
+++ notes-app-back-end/src/handler.js
@@ -0,0 +1,44 @@
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
+
+const addNoteHandler = (req, h) => {
+  const { title, tags, body } = req.payload;
+
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title,
+    tags,
+    body,
+    id,
+    createdAt,
+    updatedAt,
+  };
+
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const res = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    });
+    res.code(201);
+    return res;
+  }
+
+  const res = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  res.code(500);
+  return res;
+};
+
+module.exports = { addNoteHandler };
