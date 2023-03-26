191358f416805eeca66b4e651957d70fb7f63896
Okyan Awang Ramadhana <okyanawang@gmail.com>
2023-03-26 08:42:09 +0700

    Add deleteNoteByID Feature

--- notes-app-back-end/src/handler.js
+++ notes-app-back-end/src/handler.js
@@ -103,9 +103,33 @@ const editNoteByIdHandler = (req, h) => {
   return res;
 };
 
+const deleteNoteByIdHandler = (req, h) => {
+  const { id } = req.params;
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes.splice(index, 1);
+    const res = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+    res.code(200);
+    return res;
+  }
+
+  const res = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
+  });
+  res.code(404);
+  return res;
+};
+
 module.exports = {
   addNoteHandler,
   getAllNotesHandler,
   getNoteByIdHandler,
   editNoteByIdHandler,
+  deleteNoteByIdHandler,
 };
