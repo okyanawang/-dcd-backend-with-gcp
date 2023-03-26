e896078c5f854215377f705e19bf7faa9368f89d
Okyan Awang Ramadhana <okyanawang@gmail.com>
2023-03-26 14:05:22 +0700

    Build bookshelf API

---
+++ bookshelf-api/src/handler.js
@@ -0,0 +1,211 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const getAllBooksHandler = () => ({
+  status: 'success',
+  data: {
+    books: books.map((book) => ({
+      id: book.id,
+      name: book.name,
+      publisher: book.publisher,
+    })),
+    // books: books.length > 0 ? books : [],
+  },
+});
+
+const addBookHandler = (req, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = req.payload;
+
+  const id = nanoid(16);
+  const finished = pageCount === readPage;
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  const a = name !== undefined;
+  const b = readPage <= pageCount;
+
+  const isSuccess = a && b;
+
+  if (isSuccess) {
+    const res = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    books.push(newBook);
+    res.code(201);
+    return res;
+  }
+  if (!b) {
+    const res = h.response({
+      status: 'fail',
+      message:
+        'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    res.code(400);
+    return res;
+  }
+  if (!a) {
+    const res = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    res.code(400);
+    return res;
+  }
+
+  const res = h.response({
+    status: 'fail',
+    message: 'Buku gagal ditambahkan',
+  });
+  res.code(500);
+  return res;
+};
+
+const getBookByIdHandler = (req, h) => {
+  const { id } = req.params;
+
+  const book = books.filter((n) => n.id === id)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const res = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  res.code(404);
+  return res;
+};
+
+const editBookByIdHandler = (req, h) => {
+  const { id } = req.params;
+
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = req.payload;
+  const updatedAt = new Date().toISOString();
+
+  const index = books.findIndex((book) => book.id === id);
+
+  if (index !== -1) {
+    books[index] = {
+      ...books[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      reading,
+      updatedAt,
+    };
+
+    const a = name !== undefined;
+    const b = readPage <= pageCount;
+
+    if (!a) {
+      const res = h.response({
+        status: 'fail',
+        message: 'Gagal memperbarui buku. Mohon isi nama buku',
+      });
+      res.code(400);
+      return res;
+    }
+
+    if (!b) {
+      const res = h.response({
+        status: 'fail',
+        message:
+          'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+      });
+      res.code(400);
+      return res;
+    }
+
+    const res = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui',
+    });
+    res.code(200);
+    return res;
+  }
+
+  const res = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui buku. Id tidak ditemukan',
+  });
+  res.code(404);
+  return res;
+};
+
+const deleteBookByIdHandler = (req, h) => {
+  const { id } = req.params;
+
+  const index = books.findIndex((book) => book.id === id);
+
+  if (index !== -1) {
+    books.splice(index, 1);
+    const res = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    });
+    res.code(200);
+    return res;
+  }
+
+  const res = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan',
+  });
+  res.code(404);
+  return res;
+};
+
+module.exports = {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+};
