0d6191af3bdb6a89ef46023ba39fdbbb354746f8
Okyan Awang Ramadhana <okyanawang@gmail.com>
2023-03-26 07:43:31 +0700

    Implement CORS

--- notes-app-back-end/src/routes.js
+++ notes-app-back-end/src/routes.js
@@ -1,8 +1,15 @@
+const { addNoteHandler } = require('./handler');
+
 const routes = [
   {
     method: 'POST',
     path: '/notes',
-    handler: () => {},
+    handler: addNoteHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   },
 ];
 
